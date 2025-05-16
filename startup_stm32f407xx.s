69e2f4c8dd5809f2092c02be5e02a7a5e19789c9
Alex Muraru <alex.muraru97@gmail.com>
2025-05-16 14:10:33 +0300

    Init ccmram with 0 like bss

--- startup_stm32f407xx.s
+++ startup_stm32f407xx.s
@@ -94,6 +94,20 @@ LoopFillZerobss:
   cmp r2, r4
   bcc FillZerobss
 
+/* Zero fill the ccmram segment */
+  ldr r2, =_sccmram
+  ldr r4, =_eccmram
+  movs r3, #0
+  b LoopFillZeroCCMRAM
+
+FillZeroCCMRAM:
+  str  r3, [r2]
+  adds r2, r2, #4
+
+LoopFillZeroCCMRAM:
+  cmp r2, r4
+  bcc FillZeroCCMRAM
+  
 /* Call static constructors */
     bl __libc_init_array
 /* Call the application's entry point.*/
