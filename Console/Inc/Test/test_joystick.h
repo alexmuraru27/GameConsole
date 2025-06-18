913e284d7853a500eb62285ff15d495df2097e18
Alex Muraru <alex.muraru97@gmail.com>
2025-06-18 12:23:08 +0300

    Remove test joystick

--- Console/Inc/Test/test_joystick.h
+++
@@ -1,6 +0,0 @@
-#ifndef __TEST_JOYSTICK_H
-#define __TEST_JOYSTICK_H
-
-void testJoystick(void);
-
-#endif /* __TEST_JOYSTICK_H */
--- Console/Src/Test/test_joystick.c
+++
@@ -1,5 +0,0 @@
-#include "test_joystick.h"
-
-void testJoystick(void)
-{
-}
