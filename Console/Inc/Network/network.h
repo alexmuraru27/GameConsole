fb4ff2e50a0da8920ee29d02582484036bac4d75
Alex Muraru <alex.muraru97@gmail.com>
2025-06-26 20:25:16 +0300

    Add network files skeleton

---
+++ Console/Inc/Network/network.h
@@ -0,0 +1,5 @@
+#ifndef __NETWORK_H
+#define __NETWORK_H
+#include "network_constants.h"
+
+#endif /* __NETWORK_H */
---
+++ Shared/Esp01s/network_constants.h
@@ -0,0 +1,12 @@
+#ifndef __NETWORK_H
+#define __NETWORK_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif
