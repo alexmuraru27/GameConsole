52346dd5f9494b41b78e965b48284799ee31894c
Alex Muraru <alex.muraru97@gmail.com>
2026-06-15 18:30:39 +0300

    Sync main example

--- Console/Inc/console_config.h
+++ Console/Inc/console_config.h
@@ -1,6 +1,7 @@
 #ifndef __CONSOLE_CONFIG_H
 #define __CONSOLE_CONFIG_H
 
-#define RENDERER_TESTING
+// #define RENDERER_TESTING
+// #define MAIN_SYNC_FPS
 
 #endif /* __CONSOLE_CONFIG_H */
--- Console/Src/main.c
+++ Console/Src/main.c
@@ -1,23 +1,79 @@
-#include "console_config.h"
+#include <stm32f407xx.h>
 #include "sysclock.h"
+#include "renderer.h"
 #include "game_console.h"
+#include "stddef.h"
+#include "string.h"
+#include "main_menu.h"
+#include "stdio.h"
 #include "buzzer.h"
+#include "console_config.h"
 #include "renderer_testing.h"
+
+#define FPS 60
+#define FRAME_PERIOD (1000U / FPS)
+uint32_t s_last_frame_time = 0U;
 
 void SystemInit(void)
 {
     systemClockConfig();
 }
 
+static void syncFrame()
+{
+    if (getSysTime() - s_last_frame_time < FRAME_PERIOD)
+    {
+        // Busy-wait until it's time for the next frame
+        while ((getSysTime() - s_last_frame_time) < FRAME_PERIOD)
+            ;
+    }
+    s_last_frame_time = getSysTime(); // Keep consistent frame timing
+}
+
+static void init()
+{
+    // mainMenuInit();
+#ifdef RENDERER_TESTING
+    rendererTestingInit();
+#endif
+}
+
+static void update()
+{
+    // mainMenuUpdate();
+#ifdef RENDERER_TESTING
+    rendererTestingUpdate();
+#endif
+}
+
+static void render()
+{
+    // rendererRender();
+#ifdef RENDERER_TESTING
+    rendererTestingRender();
+#endif
+}
+
 int main(void)
 {
     buzzerSetMute(true);
     gameConsoleInit();
-    rendererTestingInit();
 
+    init();
+    printf("Boot OK\n");
+    int i = 0;
     while (1)
     {
-        rendererTestingUpdate();
-        rendererTestingRender();
+        if (i % (FPS * 10) == 0)
+        {
+            printf("Heartbeat %ds\n", i / FPS);
+        }
+        i++;
+        update();
+        render();
+
+#ifdef MAIN_SYNC_FPS
+        syncFrame();
+#endif
     }
 }
